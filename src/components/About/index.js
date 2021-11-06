51d23be7a87b7169efc1e8a195f42d5a11c92a98
Duncan Byrne <duncanbyrne@Duncans-MacBook-Pro.local>
2021-11-05 21:06:02 -0400

    initial commit

---
+++ src/components/About/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section className="my-5">
+      <div className="my-2">
+        <div className="profile-img my-5">
+        {/* <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i> */}
+        </div>
+        <p>
+        Hello! My name is Duncan Byrne, I am a web developer from Long Island, New York. I have been coding and building websites for almost two years. I enjoy the process of learning
+        different technologies, and putting them to work. I hope to one day work on developing video games, building financial projects and overall just having fun while coding.
+        </p>
+        <p>
+        I have recently graduated from the University of Columbia's coding bootcamp where I learned in demand skills, to advance my career going forward. Overall I am looking to land a job where I can 
+        continue to learn and grow as a developer, while working on great projects with great people!
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
